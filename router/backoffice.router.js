ef830264a5ef4ba9788472989a87978cc24f95c1
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-03-11 11:46:32 +0100

    Finalisation du router backoffice

--- router/backoffice.router.js
+++ router/backoffice.router.js
@@ -46,6 +46,40 @@ Defintiion
                 })
             })
 
+            // Define backoffice route to display form to create new post
+            this.router.get('/post/create', (req, res) => {
+                // Render edit vue with data
+                return res.render('create', { 
+                    msg: 'Display vue create', 
+                    method: req.method,
+                    err: null, 
+                    data: { title:undefined, content: undefined },
+                    url: req.originalUrl,
+                    status: 200
+                })
+            })
+
+            // Define backoffice route to create new post
+            this.router.post('/post/create', (req, res) => {
+                // Get all posts from the BDD
+                Controllers.post.createOne(req)
+                .then( apiResponse => {
+                    // Redirect to edit page
+                    return res.redirect(`/post/edit/${apiResponse._id}`)
+                })
+                .catch( apiError => {
+                    // Render create vue with error
+                    return res.render('create', { 
+                        msg: 'Posts not created', 
+                        method: req.method,
+                        err: apiError, 
+                        data: { title: undefined, content: undefined },
+                        url: req.originalUrl,
+                        status: 404
+                    })
+                })
+            })
+
             // Define backoffice route to display edit vue
             this.router.get('/post/edit/:id', (req, res) => {
                 // Get all posts from the BDD
@@ -65,6 +99,52 @@ Defintiion
                     // Render edit vue with error
                     return res.render('edit', { 
                         msg: 'Post not found', 
+                        method: req.method,
+                        err: apiError, 
+                        data: null,
+                        url: req.originalUrl,
+                        status: 404
+                    })
+                })
+            })
+
+            // Define backoffice route to update a post
+            this.router.post('/post/edit/:id', (req, res) => {
+                // Get all posts from the BDD
+                Controllers.post.updateOne(req)
+                .then( apiResponse => {
+                    console.log(apiResponse)
+
+                    // Fetch post
+                    Controllers.post.readOne(req)
+                    .then( postData => {
+                        // Render edit vue with data
+                        return res.render('edit', { 
+                            msg: 'Post found', 
+                            method: req.method,
+                            err: null, 
+                            data: postData,
+                            url: req.originalUrl,
+                            status: 200
+                        })
+                    })
+                    .catch( postError => {
+                        // Render edit vue with data
+                        return res.render('edit', { 
+                            msg: 'Post not found', 
+                            method: req.method,
+                            err: postError, 
+                            data: null,
+                            url: req.originalUrl,
+                            status: 200
+                        })
+                    })
+                    
+                })
+                .catch( apiError => {
+                    // Render edit vue with error
+                    return res.render('edit', { 
+                        msg: 'Post not updated', 
                         method: req.method,
                         err: apiError, 
                         data: null,
